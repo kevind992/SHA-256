399e902eb13605feb32ad6e74d635fc9c0b5f8e6
Kevin <kevind992@hotmail.com>
2019-03-18 16:30:00 +0000

    Part 3 of Padding Complete

--- padfile.c
+++ padfile.c
@@ -7,6 +7,8 @@ union msgblock {
     uint64_t s[8];
 };
 
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main (int argc, char *argv[]){
     
     union msgblock M;
@@ -14,13 +16,18 @@ int main (int argc, char *argv[]){
     uint64_t nobytes;
     uint64_t nobits = 0;
 
+    enum status S = READ;
+
     FILE* f;
+
+    int i;
 
     f = fopen(argv[1], "r");
 
-    while (!feof(f)){
+    while (S == READ){
 
         nobytes = fread(M.e, 1, 64, f);
+        printf("Read %2llu bytes\n", nobytes);
         nobits = nobits + (nobytes * 8);
 
         if(nobytes < 56){
@@ -31,7 +38,27 @@ int main (int argc, char *argv[]){
                 M.e[nobytes] = 0x00;
             }
             M.s[7] = nobits;
+            S = FINISH;
+        } else if (nobytes < 64){
+            S = PAD0;
+            M.e[nobytes] = 0x80;
+            while (nobytes < 64) {
+                nobytes = nobytes + 1;
+                M.e[nobytes] = 0x00;
+            }
+        } else if (feof(f)) {
+            S = PAD1;
         }
+    }
+
+    if (S == PAD0 || S == PAD1) {
+        for(i = 0; i < 56; i++){
+            M.e[i] = 0x00;
+        }
+        M.s[7] = nobits;
+    } 
+    if ( S == PAD1){
+        M.e[0] = 0x80;
     }
 
     fclose(f);
@@ -42,6 +69,5 @@ int main (int argc, char *argv[]){
     }
     printf("\n");
     
-
     return 0;
 }
